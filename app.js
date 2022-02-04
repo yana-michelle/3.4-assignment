5ed9486549e536233ca543ec12dc487415b1e1a4
yana-michelle <60404658+yana-michelle@users.noreply.github.com>
2022-02-04 17:09:57 -0600

    creations

---
+++ app.js
@@ -0,0 +1,12 @@
+//function name(){
+    //what you want to be done in here;
+//}
+// name();
+//"()" = parameters
+const name ="Adrianna"
+
+function adder(num1, num2){
+    console.log(num1 + num2);
+}
+
+adder(7, 36);
