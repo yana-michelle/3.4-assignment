fddad39059ad78eb93fe3652d2981b28465886b1
yana-michelle <60404658+yana-michelle@users.noreply.github.com>
2022-02-04 17:45:39 -0600

    object

--- app.js
+++ app.js
@@ -14,6 +14,23 @@ const name = "Adrianna";
 
 function toUpper(text){
     const upperCased = text.toUpperCase();
-    console.log(text);
+    console.log(upperCased);
 }
-toUpper(name)
+toUpper(name);
+
+const favYoutuber = "Markiplier";
+
+const toLower = (text) => {
+    const lowCased = text.toLowerCase();
+    console.log(lowCased);
+}
+toLower(favYoutuber);
+
+const youtuber = {firstName:"Mark", lastName:"Fischbach", youtubeName:"Markiplier", age:32,
+    fullName: function(){
+        return this.firstName+" "+this.lastName;
+    }
+};
+
+document.getElementById("demo").innerHTML = youtuber.fullName();
+document.getElementById("demo").innerHTML = text.favYoutuver();
